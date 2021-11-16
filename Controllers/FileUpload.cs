f4a242495c696aad512d3fdd0d228a2ea65ee63d
ssurve_swiftwin <ssurve@swiftwininc.com>
2021-11-16 19:42:47 +0530

    Add project files.

---
+++ Controllers/FileUpload.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZipFileUploadDownload.Controllers
+{
+    public class FileUpload : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("FileUpload")]
+        public async Task<IActionResult> Index(List<IFormFile> files)
+        {
+
+
+            //string startPath = @"c:\example\start";
+            //string zipPath = @"c:\example\result.zip";
+            //string extractPath = @"c:\example\extract";
+
+            //System.IO.Compression.ZipFile.CreateFromDirectory(startPath, zipPath);
+            //System.IO.Compression.ZipFile.ExtractToDirectory(zipPath, extractPath);
+
+
+
+
+
+
+
+            long size = files.Sum(f => f.Length);
+
+            var filePaths = new List<string>();
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    // full path to file in temp location
+                    var filePath = Path.GetTempFileName(); //we are using Temp file name just for the example. Add your own file path.
+                    filePaths.Add(filePath);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+            return Ok(new { count = files.Count, size, filePaths });
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ZipFileUploadDownload.Models;
+
+namespace ZipFileUploadDownload.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IHostingEnvironment _hostingEnvironment;
+        public HomeController(ILogger<HomeController> logger, IHostingEnvironment hostingEnvironment)
+        {
+            _logger = logger;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public FileResult GenerateAndDownloadZip()
+        {
+            var webRoot = _hostingEnvironment.WebRootPath;
+            var fileName = "Output.zip";
+            var tempOutput = webRoot + "/Assets/" + fileName;
+            using (ZipOutputStream zipOutput = new ZipOutputStream(System.IO.File.Create(tempOutput)))
+            {
+                zipOutput.SetLevel(9);
+                byte[] buffer = new byte[4096];
+                var pdfList = new List<string>();
+                pdfList.Add(webRoot + @"/Assets/Cerificate of Incorporation (3) (1).PDF");
+                pdfList.Add(webRoot + @"/Assets/Cerificate of Incorporation(1)1.PDF");
+                pdfList.Add(webRoot + @"/Assets/Certificate of Incorporation (10).PDF");
+                pdfList.Add(webRoot + @"/Assets/Form ADT-1-01042021_signed.pdf");
+                pdfList.Add(webRoot + @"/Assets/Form ADT-1-05122020_signed.pdf");
+                pdfList.Add(webRoot + @"/Assets/Form ADT-1-06022018_signed.pdf");
+                pdfList.Add(webRoot + @"/Assets/Form AOC-4-01122018_signed.pdf");
+                pdfList.Add(webRoot + @"/Assets/Form AOC-4-03012019_signed.pdf");
+                pdfList.Add(webRoot + @"/Assets/Form AOC-4-04022020_signed.pdf");
+                pdfList.Add(webRoot + @"/Assets/Form DIR-12-01022019_signed(1).pdf");
+                pdfList.Add(webRoot + @"/Assets/Form DIR-12-01042020_signed.pdf");
+                pdfList.Add(webRoot + @"/Assets/Form DIR-12-01062020_signed.pdf");
+                pdfList.Add(webRoot + @"/Assets/Form MGT-7-01012020_signed (2).pdf");
+                pdfList.Add(webRoot + @"/Assets/Form MGT-7-01102018_signed.pdf");
+                pdfList.Add(webRoot + @"/Assets/Form MGT-7-02012020_signed (2) (1).pdf");
+                pdfList.Add(webRoot + @"/Assets/Form PAS-3-01012019_signed.pdf");
+                pdfList.Add(webRoot + @"/Assets/Form PAS-3-01082019_signed.pdf");
+                pdfList.Add(webRoot + @"/Assets/Form PAS-3-02032019_signed.pdf");
+                pdfList.Add(webRoot + @"/Assets/Form PAS-6-04112020_signed.pdf");
+                pdfList.Add(webRoot + @"/Assets/Form PAS-6-12092020_signed.pdf");
+                pdfList.Add(webRoot + @"/Assets/Form PAS-6-11092020_signed.pdf");
+                pdfList.Add(webRoot + @"/Assets/Fresh Certificate of Incorporation Consequent upon  Change of Name-080311 (1).PDF");
+                pdfList.Add(webRoot + @"/Assets/Fresh Certificate of Incorporation Consequent upon Change of Name on Conversion to Private Limited Company-130310 (2).PDF");
+                pdfList.Add(webRoot + @"/Assets/Fresh Certificate of Incorporation Consequent upon Change of Name on Conversion to Private Limited Company-130310.PDF");
+
+                for (int count = 0; count < pdfList.Count; count++)
+                {
+                    ZipEntry entry = new ZipEntry(Path.GetFileName(pdfList[count]));
+                    entry.DateTime = DateTime.Now;
+                    entry.IsUnicodeText = true;
+                    zipOutput.PutNextEntry(entry);
+                    using (FileStream fileStream = System.IO.File.OpenRead(pdfList[count]))
+                    {
+                        int sourceBytes;
+                        do
+                        {
+                            sourceBytes = fileStream.Read(buffer, 0, buffer.Length);
+                            zipOutput.Write(buffer, 0, sourceBytes);
+                        } while (sourceBytes > 0);
+
+                    }
+                }
+                zipOutput.Finish();
+                zipOutput.Flush();
+                zipOutput.Close();
+            }
+            byte[] finalResult = System.IO.File.ReadAllBytes(tempOutput);
+            if (System.IO.File.Exists(tempOutput))
+            {
+                System.IO.File.Delete(tempOutput);
+            }
+            if(finalResult == null || !finalResult.Any())
+            {
+                throw new Exception(String.Format("Nothing Found"));
+            }
+            return File(finalResult, "application/zip", fileName);
+        }
+    }
+}
